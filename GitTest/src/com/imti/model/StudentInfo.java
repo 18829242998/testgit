3c413ac8a93b8aedf1bb7c9fabd94ddca20f9c57
guoHaiFeng <1191619897@qq.com>
2020-10-24 14:17:07 +0800

    提交项目第一次

---
+++ GitTest/src/com/imti/model/StudentInfo.java
@@ -0,0 +1,25 @@
+package com.imti.model;
+/**@文件名: StudentInfo.java
+ * @类功能说明: 
+ * @作者: GuoHaiFeng
+ * @Email: 1191619897@qq.com
+ * @日期: 2020年10月24日下午2:15:40
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: GuoHaiFeng</li> 
+ * 	 <li>日期: 2020年10月24日下午2:15:40</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class StudentInfo {
+
+	private String stuName;
+
+	public String getStuName() {
+		return stuName;
+	}
+
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+}
